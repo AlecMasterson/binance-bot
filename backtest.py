02c1d375f21d11a4d464ac8c5cf41c1fb62da571
Alec Masterson <alecjmasterson@gmail.com>
2018-03-01 16:28:34 -0600

    Moved buying and selling functionality to the Wallet class.

--- backtest.py
+++ backtest.py
@@ -6,25 +6,13 @@ class BackTest:
 		self.candles = candles
 		self.person = Wallet(0.08, 0)
 
-	def buy(self, candle):
-		if (self.person.btc_balance > 0):
-			print "Buying at {} with {} BTC to get {} ETH".format(candle.open_price, self.person.btc_balance, self.person.btc_balance * candle.open_price)
-			self.person.eth_balance = self.person.btc_balance * candle.open_price
-			self.person.btc_balance = 0
-
-	def sell(self, candle):
-		if (self.person.eth_balance > 0):
-			print "Selling at {} with {} ETH to get {} BTC".format(candle.open_price, self.person.eth_balance, self.person.eth_balance / candle.open_price)
-			self.person.btc_balance = self.person.eth_balance / candle.open_price
-			self.person.eth_balance = 0
-
 	def test(self):
 		for index, candle in enumerate(self.candles):
 			if index < 3:
 				continue
 			if self.candles[index-2].change < 0 and self.candles[index-1].change < 0 and candle.change > 0:
-				self.buy(candle)
+				self.person.buy(candle.open_price)
 			if self.candles[index-2].change > 0 and self.candles[index-1].change > 0 and candle.change < 0:
-				self.sell(candle)
+				self.person.sell(candle.open_price)
 
 		print("BTC: ", self.person.btc_balance, " ETH: ", self.person.eth_balance)
