caa6ba7f1f8b63983448b226e38c67da96b54755
Alec Masterson <alecjmasterson@gmail.com>
2018-03-21 16:33:22 -0500

    Massive Update. Idk what all is in it. Just focus on get_history and backtest

--- backtest.py
+++ backtest.py
@@ -1,18 +1,130 @@
-from wallet import Wallet
+import pandas, sys, math
+import matplotlib.pyplot as plt
+import numpy as np
 
-class BackTest:
+def round_down(x):
+	return float(math.floor(x * 1000) / 1000)
 
-	def __init__(self, candles):
-		self.candles = candles
-		self.person = Wallet(0.08, 0)
+def buy(trading, row):
+	return trading.append(
+		{
+			"Type": "buy",
+			"Close Time": row["Close Time"],
+			"Close": row["Close"],
+			"BTC": trading.iloc[-1]["BTC"] - round_down(trading.iloc[-1]["BTC"]),
+			"ETH": trading.iloc[-1]["ETH"] + (round_down(trading.iloc[-1]["BTC"]) / row["Close"])*0.999
+		}, ignore_index=True
+	)
 
-	def test(self):
-		for index, candle in enumerate(self.candles):
-			if index < 3:
-				continue
-			if self.candles[index-2].change < 0 and self.candles[index-1].change < 0 and candle.change > 0:
-				self.person.buy(candle.open_price)
-			if self.candles[index-2].change > 0 and self.candles[index-1].change > 0 and candle.change < 0:
-				self.person.sell(candle.open_price)
+def sell(trading, row):
+	return trading.append(
+		{
+			"Type": "sell",
+			"Close Time": row["Close Time"],
+			"Close": row["Close"],
+			"BTC": trading.iloc[-1]["BTC"] + (round_down(trading.iloc[-1]["ETH"]) * row["Close"])*0.999,
+			"ETH": trading.iloc[-1]["ETH"] - round_down(trading.iloc[-1]["ETH"])
+		}, ignore_index=True
+	)
 
-		print("BTC: ", self.person.btc_balance, " ETH: ", self.person.eth_balance)
+def predict_sell(trading, val, args):
+	change = trading.iloc[len(trading.index) - 2]["BTC"] / (trading.iloc[-1]["BTC"] + (round_down(trading.iloc[-1]["ETH"]) * val)*0.999)
+	if change < args["low"] or change > args["hi"]:
+		return True
+	return False
+
+def backtest(file_name, interval_name, args):
+
+	# --------------------------------------------------------------------------
+	# SETUP
+	# --------------------------------------------------------------------------
+
+	print("\nPROC: Back-Test " + interval_name + " Interval Data...\n")
+
+	try:
+		print("IO: Looking For Existing Data...")
+		df = pandas.read_csv(file_name)
+		print("Found!")
+	except:
+		print("ERROR: No Existing Data Found!")
+		sys.exit()
+
+	trading = pandas.DataFrame(
+		data={"Type": "sell", "Close Time": 0, "Close": 0, "BTC": 0.04, "ETH": 0.0},
+		index=[0]
+	)
+
+	# --------------------------------------------------------------------------
+	# ANALYSIS
+	# --------------------------------------------------------------------------
+
+	for index, row in df.iterrows():
+		if index > 2:
+			section = df[index-3:index]
+			line = np.poly1d(np.polyfit(section["Close Time"], section["Close"], 2))
+
+			if line.c[0] < 0 and row["Close"] < df.iloc[index-3]["Close"] and line.c[0] < float(args["thin"] * -1/100000000000000000):
+				if row["Close"] < df.iloc[index-1]["Close"] and trading.iloc[-1]["Type"] is "sell":
+					trading = buy(trading, row)
+				if row["Close"] > df.iloc[index-1]["Close"] and trading.iloc[-1]["Type"] is "buy" and predict_sell(trading, row["Close"], args):
+					trading = sell(trading, row)
+
+				# Additional Plotting
+				plot_x = np.linspace(section.iloc[0]["Close Time"], section.iloc[len(section.index)-1]["Close Time"])
+				plt.plot(pandas.to_datetime(plot_x, unit="ms"), line(plot_x))
+
+			if line.c[0] > 0 and row["Close"] > df.iloc[index-3]["Close"] and line.c[0] > float(args["thin"] * 1/100000000000000000):
+				if row["Close"] < df.iloc[index-1]["Close"] and trading.iloc[-1]["Type"] is "sell":
+					trading = buy(trading, row)
+				if row["Close"] > df.iloc[index-1]["Close"] and trading.iloc[-1]["Type"] is "buy" and predict_sell(trading, row["Close"], args):
+					trading = sell(trading, row)
+
+				# Additional Plotting
+				plot_x = np.linspace(section.iloc[0]["Close Time"], section.iloc[len(section.index)-1]["Close Time"])
+				plt.plot(pandas.to_datetime(plot_x, unit="ms"), line(plot_x))
+
+	# --------------------------------------------------------------------------
+	# SAVE ANALYSIS
+	# --------------------------------------------------------------------------
+
+	try:
+		print("IO: Writing " + interval_name + " Back-Tested Data to backtest_" + interval_name + ".csv")
+		trading.to_csv("backtest_" + interval_name + ".csv", columns=["Type", "Close Time", "Close", "BTC", "ETH"], index=False)
+		print("Success!")
+	except:
+		print("ERROR: Failed Writing to File!")
+		sys.exit()
+
+	# --------------------------------------------------------------------------
+	# RESULTS
+	# --------------------------------------------------------------------------
+
+	print("\nRESULTS...\n")
+	print("BTC: "+str(trading.iloc[-1]["BTC"])+"\nETH: "+str(trading.iloc[-1]["ETH"]))
+	final = trading.iloc[-1]["BTC"] + (trading.iloc[-1]["ETH"] * trading.iloc[-1]["Close"])
+	print("Final in BTC: "+str(final)+"\nPercent Increase: "+str(final/0.04))
+
+	# --------------------------------------------------------------------------
+	# PLOTTING
+	# --------------------------------------------------------------------------
+
+	df["Close Time"] = pandas.to_datetime(df["Close Time"], unit="ms")
+	trading["Close Time"] = pandas.to_datetime(trading["Close Time"], unit="ms")
+
+	bought = pandas.DataFrame(columns=["Close Time", "Close"])
+	sold = pandas.DataFrame(columns=["Close Time", "Close"])
+	for index, row in trading.iloc[1:].iterrows():
+		if row["Type"] is "buy":
+			bought = bought.append(row, ignore_index=True)
+		else:
+			sold = sold.append(row, ignore_index=True)
+
+	x, y = bought.as_matrix(["Close Time", "Close"]).T
+	a, b = sold.as_matrix(["Close Time", "Close"]).T
+
+	plt.plot(df["Close Time"], df["Close"], linestyle="dashed")
+	plt.scatter(x, y, color="red")
+	plt.scatter(a, b, color="green")
+	plt.show()
+
+backtest("data_hour.csv", "1-Hour", {"thin": 2, "low": 0.998, "hi": 1.05})
--- get_history.py
+++ get_history.py
@@ -1,6 +1,5 @@
 from binance.client import Client
-import pandas
-import sys, time
+import pandas, sys, time
 
 try:
 	client = Client("", "")
@@ -8,46 +7,59 @@ except:
 	print("ERROR: Could Not Connect to API Client!")
 	sys.exit()
 
+def create_frame(data):
+	return pandas.DataFrame(
+		data,
+		columns=[
+			"Open Time", "Open", "High", "Low",
+			"Close", "Volume", "Close Time", "Quote Asset Volume",
+			"Number Trades", "Taker Base Asset Volume", "Take Quote Asset Volume", "Ignore"
+		]
+	)
+
 def get_data(file_name, interval_name, interval_api):
-	
+
 	print("\nPROC: Getting " + interval_name + " Interval Data...\n")
-	
+
+	df = create_frame(None)
+
 	try:
 		print("IO: Looking For Existing Data...")
-		frame_old = pandas.read_csv(file_name).set_index("Open Time")
-		start_time = frame_old.index[-2]
+		df = pandas.concat([df, pandas.read_csv(file_name)]).drop_duplicates(subset="Open Time").sort_values(by=["Open Time"]).reset_index(drop=True)
+		start_time = df.iloc[-2]["Open Time"]
 		print("Found!")
 	except:
 		print("WARN: No Existing Data Found!")
-		start_time = "1 Dec, 2017"
-	end_time = str(int(round(time.time() * 1000)))
+		start_time = 1512108000000
+	end_time = int(round(time.time() * 1000))
 
 	try:
 		print("API: Get " + interval_name + " Interval Data...")
-		data = client.get_historical_klines("ETHBTC", interval_api, str(start_time), str(end_time))
-		frame = pandas.DataFrame(
-			data,
-			columns=[
-				"Open Time", "Open", "High", "Low",
-				"Close", "Volume", "Close Time", "Quote Asset Volume",
-				"Number Trades", "Taker Base Asset Volume", "Take Quote Asset Volume", "Ignore"
-			]
-		).set_index("Open Time")
+
+		while start_time < end_time:
+			current_end = start_time + 259200000
+			if start_time + 259200000 > end_time:
+				current_end = end_time
+
+			print("\tStart Time: " + str(start_time) + "\tEnd Time: " + str(current_end))
+
+			df = pandas.concat([
+				df,
+				create_frame(client.get_historical_klines("ETHBTC", interval_api, str(start_time), str(current_end)))
+			]).drop_duplicates(subset="Open Time").sort_values(by=["Open Time"]).reset_index(drop=True)
+			start_time += 259200000
+			time.sleep(4)
 		print("Success!")
 	except:
 		print("ERROR: Failed API Call!")
 		sys.exit()
 
-	if start_time != "1 Dec, 2017":
-		frame = pandas.concat([frame, frame_old]).drop_duplicates(["Open Time"])
-
 	try:
 		print("IO: Writing " + interval_name + " Interval Data to " + file_name)
-		frame.to_csv(file_name)
+		df.to_csv(file_name, index=False)
 		print("Success!")
 	except:
 		print("ERROR: Failed Writing to File!")
 		sys.exit()
-	
-get_data("data_minute.csv", "30-Minute", Client.KLINE_INTERVAL_30MINUTE)
-#get_data("data_hour.csv", "1-Hour", Client.KLINE_INTERVAL_1HOUR)
+
+get_data("data_hour.csv", "1-Hour", Client.KLINE_INTERVAL_1HOUR)
