1078ac598e157f9be88dd3f68f509e71822eb463
Alec Masterson <alecjmasterson@gmail.com>
2018-03-23 15:16:36 -0500

    Code Cleanup and commenting. Yaaaaay commenting.

--- get_history.py
+++ get_history.py
@@ -1,65 +1,111 @@
 from binance.client import Client
-import pandas, sys, time
+import pandas, sys, time, datetime
 
-try:
-	client = Client("", "")
-except:
-	print("ERROR: Could Not Connect to API Client!")
-	sys.exit()
+# ------------------------------------------------------------------------------
+# HELPER FUNCTIONS
+# ------------------------------------------------------------------------------
 
+# Create a frame to store historical data from API
 def create_frame(data):
 	return pandas.DataFrame(
 		data,
 		columns=[
-			"Open Time", "Open", "High", "Low",
-			"Close", "Volume", "Close Time", "Quote Asset Volume",
-			"Number Trades", "Taker Base Asset Volume", "Take Quote Asset Volume", "Ignore"
+			'Open Time', 'Open', 'High', 'Low',
+			'Close', 'Volume', 'Close Time', 'Quote Asset Volume',
+			'Number Trades', 'Taker Base Asset Volume',
+			'Take Quote Asset Volume', 'Ignore'
 		]
 	)
 
-def get_data(file_name, interval_name, interval_api):
+# Concat two similar pandas.DataFrame and maintain the correct order
+def combine_frames(df, df2):
+	return pandas.concat(
+		[df, df2]
+	).drop_duplicates(
+		subset='Open Time'
+	).sort_values(
+		by=['Open Time']
+	).reset_index(drop=True)
 
-	print("\nPROC: Getting " + interval_name + " Interval Data...\n")
+# ------------------------------------------------------------------------------
+# GET DATA FUNCTION
+# ------------------------------------------------------------------------------
+
+def get_data(fileName, intervalAPI):
+
+	print('\nPROC: Getting Historical Data for ' + fileName + '\n')
+
+	# --------------------------------------------------------------------------
+	# PREVIOUSLY STORED DATA
+	# --------------------------------------------------------------------------
 
 	df = create_frame(None)
-
 	try:
-		print("IO: Looking For Existing Data...")
-		df = pandas.concat([df, pandas.read_csv(file_name)]).drop_duplicates(subset="Open Time").sort_values(by=["Open Time"]).reset_index(drop=True)
-		start_time = df.iloc[-2]["Open Time"]
-		print("Found!")
+		print('IO: Looking For Existing Data...')
+		df = combine_frames(df, pandas.read_csv(fileName))
+		startTime = df.iloc[-2]['Open Time']
+		print('Found!')
 	except:
-		print("WARN: No Existing Data Found!")
-		start_time = 1512108000000
-	end_time = int(round(time.time() * 1000))
+		print('WARN: No Existing Data Found!')
+		# Use Dec 1, 2017 as a generic start date.
+		startTime = 1512108000000
+	endTime = int(round(time.time() * 1000))
+
+	# --------------------------------------------------------------------------
+	# GET NEW DATA
+	# --------------------------------------------------------------------------
 
 	try:
-		print("API: Get " + interval_name + " Interval Data...")
+		print('API: Get Historical Data...')
 
-		while start_time < end_time:
-			current_end = start_time + 259200000
-			if start_time + 259200000 > end_time:
-				current_end = end_time
+		while startTime < endTime:
+			# Acquire data in 3 day intervals to not overload the API.
+			curEnd = min(startTime + 259200000, endTime)
 
-			print("\tStart Time: " + str(start_time) + "\tEnd Time: " + str(current_end))
+			formatStart = str(datetime.date.fromtimestamp(startTime / 1e3))
+			formatEnd = str(datetime.date.fromtimestamp(curEnd / 1e3))
+			print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
 
-			df = pandas.concat([
-				df,
-				create_frame(client.get_historical_klines("ETHBTC", interval_api, str(start_time), str(current_end)))
-			]).drop_duplicates(subset="Open Time").sort_values(by=["Open Time"]).reset_index(drop=True)
-			start_time += 259200000
+			df = combine_frames(
+				df, create_frame(
+					client.get_historical_klines(
+						'ETHBTC', intervalAPI,
+						str(startTime), str(curEnd)
+					)
+				)
+			)
+
+			# Acquire data in 3 day intervals to not overload the API.
+			startTime += 259200000
+
+			# Wait between API calls to not overload the API.
 			time.sleep(4)
-		print("Success!")
+		print('Success!')
 	except:
-		print("ERROR: Failed API Call!")
+		print('ERROR: Failed API Call!')
 		sys.exit()
+
+	# --------------------------------------------------------------------------
+	# SAVE DATA
+	# --------------------------------------------------------------------------
 
 	try:
-		print("IO: Writing " + interval_name + " Interval Data to " + file_name)
-		df.to_csv(file_name, index=False)
-		print("Success!")
+		print('IO: Writing Historical Data to ' + fileName)
+		df.to_csv(fileName, index=False)
+		print('Success!')
 	except:
-		print("ERROR: Failed Writing to File!")
+		print('ERROR: Failed Writing to File!')
 		sys.exit()
 
-get_data("data_hour.csv", "1-Hour", Client.KLINE_INTERVAL_1HOUR)
+# ------------------------------------------------------------------------------
+# MAIN
+# ------------------------------------------------------------------------------
+
+try:
+	client = Client('', '')
+except:
+	print('ERROR: Could Not Connect to API Client!')
+	sys.exit()
+
+# Edit second argument for different data interval.
+get_data('data_hour.csv', Client.KLINE_INTERVAL_1HOUR)
