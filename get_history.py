92c422c649f7d07e1849209708ebf9da5bd6d2e4
Alec Masterson <alecjmasterson@gmail.com>
2018-04-02 11:49:34 -0500

    The get_history script now runs without any command line arguments and ahdnles multiple coinpairs.

--- get_history.py
+++ get_history.py
@@ -28,17 +28,83 @@ def combine_frames(df, df2):
 	).reset_index(drop=True)
 
 # ------------------------------------------------------------------------------
+# MAIN FUNCTION
+# ------------------------------------------------------------------------------
+
+def get_data(coin, api):
+
+	# --------------------------------------------------------------------------
+	# PREVIOUSLY STORED DATA
+	# --------------------------------------------------------------------------
+
+	df = create_frame(None)
+	try:
+		print('IO: Looking For Existing Data...')
+		df = combine_frames(df, pandas.read_csv('data/'+str(coin)+'.csv'))
+		startTime = df.iloc[-2]['Open Time']
+		print('Found!')
+	except:
+		print('WARN: No Existing Data Found!')
+		# Use Dec 1, 2017 as a default start date.
+		startTime = 1512108000000
+	endTime = int(round(time.time() * 1000))
+
+	# --------------------------------------------------------------------------
+	# GET NEW DATA
+	# --------------------------------------------------------------------------
+
+	try:
+		while startTime < endTime:
+			# Acquire data in 3 day intervals to not overload the API.
+			curEnd = min(startTime + 259200000, endTime)
+
+			formatStart = str(datetime.datetime.fromtimestamp(startTime / 1e3))
+			formatEnd = str(datetime.datetime.fromtimestamp(curEnd / 1e3))
+			print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
+
+			df = combine_frames(
+				df, create_frame(
+					client.get_historical_klines(
+						coin, api,
+						str(startTime), str(curEnd)
+					)
+				)
+			)
+
+			# Acquire data in 3 day intervals to not overload the API.
+			startTime += 259200000
+
+			# Wait between API calls to not overload the API.
+			time.sleep(3)
+	except:
+		print('ERROR: Failed API Call!\n')
+		sys.exit()
+
+	# --------------------------------------------------------------------------
+	# SAVE DATA
+	# --------------------------------------------------------------------------
+
+	try:
+		df.to_csv('data/'+str(coin)+'.csv', index=False)
+	except:
+		print('ERROR: Failed Writing to File!\n')
+		sys.exit()
+
+
+
+# ------------------------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------------
 # COMMAND USAGE VERIFICATION
 # ------------------------------------------------------------------------------
 
-if len(sys.argv) is not 2 and len(sys.argv) is not 3:
-	print(
-		'\nERROR: Command Usage ->' +
-		'\'python3 get_history.py <output-file> [start-date]\'\n'
-	)
+if len(sys.argv) > 1:
+	print('\nERROR: Command Usage -> \'python3 get_history.py\'\n')
 	sys.exit()
 
-print('\nPROC: Getting Historical Data\n')
+print('\nPROC: Getting Historical Data')
 
 # ------------------------------------------------------------------------------
 # SETUP
@@ -51,73 +117,13 @@ except:
 	sys.exit()
 
 # Manually edit this for different time intervals.
-# Follow the specification detailed in the README file.
 api = Client.KLINE_INTERVAL_1HOUR
 
-# ------------------------------------------------------------------------------
-# PREVIOUSLY STORED DATA
-# ------------------------------------------------------------------------------
+# Manually edit this for different coin pairs.
+coins = ['ETHBTC', 'BNBBTC', 'XRPBTC', 'LTCBTC', 'ADABTC']
+for coin in coins:
+	print('\nINFO: Getting Data for '+coin+' Exchange')
+	get_data(coin, api)
+	print('\nINFO: Done Getting Data for '+coin+' Exchange')
 
-df = create_frame(None)
-try:
-	print('IO: Looking For Existing Data...')
-	df = combine_frames(df, pandas.read_csv(sys.argv[1]))
-	startTime = df.iloc[-2]['Open Time']
-	print('Found!')
-except:
-	print('WARN: No Existing Data Found!')
-	if len(sys.argv) is 2:
-		# Use Dec 1, 2017 as a default start date.
-		startTime = 1512108000000
-	else:
-		startTime = int(sys.argv[2])
-endTime = int(round(time.time() * 1000))
-
-# ------------------------------------------------------------------------------
-# GET NEW DATA
-# ------------------------------------------------------------------------------
-
-try:
-	print('API: Get Historical Data...')
-
-	while startTime < endTime:
-		# Acquire data in 3 day intervals to not overload the API.
-		curEnd = min(startTime + 259200000, endTime)
-
-		formatStart = str(datetime.datetime.fromtimestamp(startTime / 1e3))
-		formatEnd = str(datetime.datetime.fromtimestamp(curEnd / 1e3))
-		print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
-
-		df = combine_frames(
-			df, create_frame(
-				client.get_historical_klines(
-					'ETHBTC', api,
-					str(startTime), str(curEnd)
-				)
-			)
-		)
-
-		# Acquire data in 3 day intervals to not overload the API.
-		startTime += 259200000
-
-		# Wait between API calls to not overload the API.
-		time.sleep(4)
-	print('Success!')
-except Exception as e:
-	print(e)
-	print('ERROR: Failed API Call!\n')
-	sys.exit()
-
-# ------------------------------------------------------------------------------
-# SAVE DATA
-# ------------------------------------------------------------------------------
-
-try:
-	print('IO: Writing Historical Data to ' + sys.argv[1])
-	df.to_csv(sys.argv[1], index=False)
-	print('Success!\n')
-except:
-	print('ERROR: Failed Writing to File!\n')
-	sys.exit()
-
-print('PROC: Done!\n')
+print('\nPROC: Done!\n')
