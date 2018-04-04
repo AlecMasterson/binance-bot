0ef059b8ef3a4ba49b32e29c7813d1a949c5e276
Alec Masterson <alecjmasterson@gmail.com>
2018-04-04 12:21:19 -0500

    optimize.py can now be imported into a separate script to use as a helper function

--- optimize.py
+++ optimize.py
@@ -3,47 +3,61 @@ from back import baye
 import pandas, sys
 
 # ------------------------------------------------------------------------------
-# COMMAND USAGE VERIFICATION
+# MAIN FUNCTION
 # ------------------------------------------------------------------------------
 
-if len(sys.argv) != 2:
-	print('\nERROR: Command Usage -> \'python3 optimize.py <data-file>\'\n')
-	sys.exit()
-
-print('\nPROC: Bayesian Optimization for '+sys.argv[1]+'\n')
-
-# ------------------------------------------------------------------------------
-# SETUP
-# ------------------------------------------------------------------------------
-
-try:
-	print('IO: Looking For Data...')
-	data = pandas.read_csv(sys.argv[1])
-	print('Found!')
-except:
-	print('\nERROR: Data Not Found!\n')
-	sys.exit()
-
-# ------------------------------------------------------------------------------
-# OPTIMIZATION
-# ------------------------------------------------------------------------------
-
-try:
-	bo = BayesianOptimization(
-		lambda arc, thinup, thindown, power, low, hi: baye(
-			data, {
-				'arc': arc, 'thinup': thinup, 'thindown': thindown, 'power': power,
-				'low': low, 'hi': hi
+def optimize(data):
+	try:
+		bo = BayesianOptimization(
+			lambda arc, thinup, thindown, power, low, hi: baye(
+				data[len(data.index)-240:].reset_index(), {
+					'arc': arc, 'thinup': thinup,
+					'thindown': thindown, 'power': power,
+					'low': low, 'hi': hi
+				}
+			), {
+				'arc': (3.0, 3.0), 'thinup': (1.00, 9.99),
+				'thindown': (-9.99, -1.00), 'power': (-22, -16),
+				'low': (0.9600, 0.9900), 'hi': (1.0001, 1.0100)
 			}
-		), {
-			'arc': (3.0, 3.0), 'thinup': (1.00, 9.99), 'thindown': (-9.99, -1.00), 'power': (-22, -16),
-			'low': (0.9600, 0.9900), 'hi': (1.0001, 1.0100)
-		}
-	)
-	bo.maximize(init_points=100, n_iter=15, kappa=2)
-except Exception as e:
-	print(e)
-	print('\nERROR: Unknown Error During Optimization!\n')
-	sys.exit()
+		)
+		bo.maximize(init_points=50, n_iter=1, kappa=2)
+		return bo.res['max']
+	except:
+		print('\nERROR: Unknown Error During Optimization!\n')
+		sys.exit()
 
-print('\nPROC: Done!\n')
+
+
+# ------------------------------------------------------------------------------
+
+
+
+if __name__ == "__main__":
+
+	# --------------------------------------------------------------------------
+	# COMMAND USAGE VERIFICATION
+	# --------------------------------------------------------------------------
+
+	if len(sys.argv) != 2:
+		print('\nERROR: Command Usage -> \'python3 optimize.py <data-file>\'\n')
+		sys.exit()
+
+	print('\nPROC: Bayesian Optimization for '+sys.argv[1]+'\n')
+
+	# --------------------------------------------------------------------------
+	# SETUP
+	# --------------------------------------------------------------------------
+
+	try:
+		print('IO: Looking For Data...')
+		data = pandas.read_csv(sys.argv[1])
+		print('Found!\n')
+	except:
+		print('\nERROR: Data Not Found!\n')
+		sys.exit()
+
+	# Actually optimize it.
+	print('\nRESULTS\n\n'+str(optimize(data)))
+
+	print('\nPROC: Done!\n')
