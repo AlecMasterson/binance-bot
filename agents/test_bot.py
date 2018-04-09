e6d0b76b7cc2157a723a9d7bae5efcc51b888e49
Trey Kind <trey.kind@returnpath.com>
2018-04-09 16:55:27 -0600

    Working graphing and random bot

--- agents/test_bot.py
+++ agents/test_bot.py
@@ -7,10 +7,14 @@ from os.path import dirname
 sys.path.append(dirname(sys.path[0]))
 
 import numpy as np
+import time
+
 from trading_env.tenv.envs.trading_env import TradingEnv
 from trading_env.tenv.envs.gens.deterministic import SineSignal
+from trading_env.tenv.envs.gens.random import RandomWalk
 
-generator = SineSignal(period_1=3, period_2=5, epsilon=1, bias=0.0)
+generator = RandomWalk(val=100, multiplier=22, bias=2)
+# generator = SineSignal(period_1=3, period_2=5, epsilon=1, bias=0.0)
 
 episode_length = 200
 trading_fee = 0.2
@@ -20,14 +24,17 @@ history_length = 6
 
 environment = TradingEnv(data_generator=generator, episode_length=episode_length, trading_fee=trading_fee, time_fee=time_fee, history_length=history_length)
 
-# environment.render()
+environment.render()
 while True:
-    action = input("Action: Buy (b) / Sell (s) / Hold (enter): ")
-    if action == 'b':
-        action = 'buy'
-    elif action == 's':
-        action = 'sell'
-    else:
-        action = 'hold'
-    print(environment.step(action))
-    # environment.render()
+    # action = input("Action: Buy (b) / Sell (s) / Hold (enter): ")
+    # if action == 'b':
+    #     action = 'buy'
+    # elif action == 's':
+    #     action = 'sell'
+    # else:
+    #     action = 'hold'
+    action = np.random.choice(a=['buy', 'sell', 'hold'], size=1, p=[0.05, 0.05, 0.9])
+    print(action)
+    environment.step(action)
+    environment.render()
+    time.sleep(0.1)
--- trading_env/tenv/envs/gens/random.py
+++ trading_env/tenv/envs/gens/random.py
@@ -5,12 +5,18 @@ class RandomWalk:
     """Random walk data generator
     """
 
-    def __init__(self, val=100.0, bias=0.0):
-        self.val, self.bias = val, bias
+    def __init__(self, val=100.0, multiplier=1, bias=0.0):
+        self.val, self.multiplier, self.bias = val, multiplier, bias
 
     def __next__(self):
-        self.val += np.random.standard_normal() + self.bias
+        self.val += np.random.standard_normal() * self.multiplier + self.bias
         return self.val
 
     def __iter__(self):
         return self
+
+    def rewind(self):
+        return self
+
+    def next(self):
+        return self.__next__()
