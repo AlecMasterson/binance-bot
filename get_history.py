300b2fc3ddd112d255f9b9da6259c16ac1b15faf
Alec Masterson <alecjmasterson@gmail.com>
2018-04-09 17:43:57 -0500

    Updated it a bit and reformatted it. Added an additional function for outside use.

--- get_history.py
+++ get_history.py
@@ -1,133 +1,72 @@
 from binance.client import Client
 import pandas, sys, time, datetime
 
-# ------------------------------------------------------------------------------
-# HELPER FUNCTIONS
-# ------------------------------------------------------------------------------
+columnStructure = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number Trades', 'Taker Base Asset Volume', 'Take Quote Asset Volume', 'Ignore']
 
-# Create a pandas.DataFrame to store historical data from Binance API
+
+# Create a DataFrame to store the historical data from the Binance API
+# data - The initial data for the DataFrame
 def create_frame(data):
-	return pandas.DataFrame(
-		data,
-		columns=[
-			'Open Time', 'Open', 'High', 'Low',
-			'Close', 'Volume', 'Close Time', 'Quote Asset Volume',
-			'Number Trades', 'Taker Base Asset Volume',
-			'Take Quote Asset Volume', 'Ignore'
-		]
-	)
+    return pandas.DataFrame(
+        data, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number Trades', 'Taker Base Asset Volume', 'Take Quote Asset Volume', 'Ignore'])
 
-# Concat two similar pandas.DataFrame structures and maintain the correct order
+
+# Concat two similar DataFrames, remove duplicates, and maintain the correct order
+# df - First DataFrame
+# df2 - Second DataFrame
 def combine_frames(df, df2):
-	return pandas.concat(
-		[df, df2]
-	).drop_duplicates(
-		subset=['Open Time']
-	).sort_values(
-		by=['Open Time']
-	).reset_index(drop=True)
-
-# ------------------------------------------------------------------------------
-# MAIN FUNCTION
-# ------------------------------------------------------------------------------
-
-def get_data(coin, client, api):
-
-	# --------------------------------------------------------------------------
-	# PREVIOUSLY STORED DATA
-	# --------------------------------------------------------------------------
-
-	df = create_frame(None)
-	try:
-		print('IO: Looking For Existing Data...')
-		df = combine_frames(df, pandas.read_csv('data/'+str(coin)+'.csv'))
-		startTime = df.iloc[-2]['Open Time']
-		print('Found! Pulling New Historical Data...')
-	except:
-		print('WARN: No Existing Data Found! Pulling Historical Data...')
-		# Use Jan 20, 2018 as a default start date.
-		startTime = 1516406400000
-	endTime = int(round(time.time() * 1000))
-
-	# --------------------------------------------------------------------------
-	# GET NEW DATA
-	# --------------------------------------------------------------------------
-
-	try:
-		while startTime < endTime:
-			# Acquire data in 3 day intervals to not overload the API.
-			curEnd = min(startTime + 259200000, endTime)
-
-			formatStart = str(datetime.datetime.fromtimestamp(startTime / 1e3))
-			formatEnd = str(datetime.datetime.fromtimestamp(curEnd / 1e3))
-			print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
-
-			df = combine_frames(
-				df, create_frame(
-					client.get_historical_klines(
-						coin, api,
-						str(startTime), str(curEnd)
-					)
-				)
-			)
-
-			# Acquire data in 3 day intervals to not overload the API.
-			startTime += 259200000
-
-			# Wait between API calls to not overload the API.
-			time.sleep(3)
-	except:
-		print('ERROR: Failed API Call!\n')
-		sys.exit()
-
-	# --------------------------------------------------------------------------
-	# SAVE DATA
-	# --------------------------------------------------------------------------
-
-	try:
-		df.to_csv('data/'+str(coin)+'.csv', index=False)
-	except:
-		print('ERROR: Failed Writing to File!\n')
-		sys.exit()
+    return pandas.concat([df, df2]).drop_duplicates(subset=['Open Time']).sort_values(by=['Open Time']).reset_index(drop=True)
 
 
+# Load the price-data DataFrame from a file, if of the correct column structure
+# fileName - The csv file that contains the price-data
+def load_file(fileName):
+    priceData = pandas.read_csv(fileName)
+    if list(priceData) != columnStructure:
+        print('ERROR: Incorrect price-data DataFrame column structure!')
+        sys.exit()
+    return priceData
 
-# ------------------------------------------------------------------------------
 
+def get_data(coinPair):
+    try:
+        df = create_frame(None)
+        client = Client('', '')
+
+        try:
+            df = combine_frames(df, load_file('data/' + str(coinPair) + '.csv'))        # Load currently stored historical data for the coinPair.
+            startTime = df.iloc[-2]['Open Time']        # Get the second to last timestamp (allow some overlap), if it exists.
+        except:
+            startTime = datetime.datetime(2018, 1, 20).timestamp() * 1000        # If no existing data was found, use "Jan 20, 2018" in milliseconds as a default start datetime.
+        endTime = datetime.datetime.now().timestamp() * 1000        # The end datetime will always be the current time in milliseconds.
+
+        while startTime < endTime:
+            curEnd = min(startTime + 259200000, endTime)        # Acquire data in 3 day intervals to not overload the API.
+
+            formatStart = str(datetime.datetime.fromtimestamp(startTime / 1e3))
+            formatEnd = str(datetime.datetime.fromtimestamp(curEnd / 1e3))
+            print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
+
+            df = combine_frames(df, create_frame(client.get_historical_klines(coinPair, Client.KLINE_INTERVAL_1HOUR, str(startTime), str(curEnd))))
+
+            startTime += 259200000        # Acquire data in 3 day intervals to not overload the API.
+            time.sleep(3)        # Wait between API calls to not overload the API.
+
+        df.to_csv('data/' + str(coinPair) + '.csv', index=False)
+
+    except Exception as e:
+        print(e)
+        print('ERROR: Unknown Error Getting Historical Data!')
+        sys.exit()
 
 
 if __name__ == "__main__":
 
-	# --------------------------------------------------------------------------
-	# COMMAND USAGE VERIFICATION
-	# --------------------------------------------------------------------------
+    if len(sys.argv) == 1:
+        print('ERROR: Command Usage -> \'python3 get_history.py [CoinPair 1] <CoinPair 2> ... <CoinPair N>\'')
+        sys.exit()
 
-	if len(sys.argv) > 1:
-		print('\nERROR: Command Usage -> \'python3 get_history.py\'\n')
-		sys.exit()
-
-	print('\nPROC: Getting Historical Data')
-
-	# --------------------------------------------------------------------------
-	# SETUP
-	# --------------------------------------------------------------------------
-
-	try:
-		client = Client('', '')
-	except:
-		print('ERROR: Could Not Connect to API Client!\n')
-		sys.exit()
-
-	# Manually edit this for different time intervals.
-	api = Client.KLINE_INTERVAL_1HOUR
-
-	# Manually edit this for different coin pairs.
-	coins = [
-		'ETHBTC', 'BNBBTC', 'XRPBTC', 'LTCBTC', 'ADABTC', 'EOSBTC', 'XLMBTC'
-	]
-	for coin in coins:
-		print('\nINFO: Getting Data for '+coin+' CoinPair')
-		get_data(coin, client, api)
-		print('\nINFO: Done Getting Data for '+coin+' CoinPair')
-
-	print('\nPROC: Done!\n')
+    for coinPair in sys.argv[1:]:
+        print('INFO: Getting Data for ' + coinPair + ' CoinPair')
+        get_data(coinPair)
+        print('INFO: Done Getting Data for ' + coinPair + ' CoinPair')
