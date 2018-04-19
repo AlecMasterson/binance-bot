714e950403253b113c5e031689b214643e5479fe
Alec Masterson <alecjmasterson@gmail.com>
2018-04-19 10:32:04 -0500

    Constants, Logging, and Get_History
    New utilities contains project constants and a logging framework.
    get_history.py now uses this.

--- get_history.py
+++ get_history.py
@@ -1,14 +1,11 @@
 from binance.client import Client
-import pandas, sys, time, datetime
-
-columnStructure = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number Trades', 'Taker Base Asset Volume', 'Take Quote Asset Volume', 'Ignore']
+import sys, utilities, pandas, datetime, time
 
 
 # Create a DataFrame to store the historical data from the Binance API
 # data - The initial data for the DataFrame
 def create_frame(data):
-    return pandas.DataFrame(
-        data, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number Trades', 'Taker Base Asset Volume', 'Take Quote Asset Volume', 'Ignore'])
+    return pandas.DataFrame(data, columns=utilities.COLUMN_STRUCTURE)
 
 
 # Concat two similar DataFrames, remove duplicates, and maintain the correct order
@@ -22,71 +19,59 @@ def combine_frames(df, df2):
 # fileName - The csv file that contains the price-data
 def load_file(fileName):
     priceData = pandas.read_csv(fileName)
-    if list(priceData) != columnStructure:
-        print('ERROR: Incorrect price-data DataFrame column structure!')
-        sys.exit()
+    if list(priceData) != utilities.COLUMN_STRUCTURE: utilities.throw_error('Incorrect price-data DataFrame column structure')
     return priceData
 
 
 # Update existing, or create new, price-data file.
 # dir - The directory to read/write data from/to
 # api - The specific time interval API to use
-# coinPair - The coinPair to get data for
-def get_data(dir, api, coinPair):
+# coinpair - The coinpair to get data for
+def get_data(dir, api, coinpair):
     try:
         df = create_frame(None)
         client = Client('', '')
 
         try:
-            df = combine_frames(df, load_file(dir + str(coinPair) + '.csv'))        # Load currently stored historical data for the coinPair.
+            df = combine_frames(df, load_file(dir + str(coinpair) + '.csv'))        # Load currently stored historical data for the coinpair.
             startTime = df.iloc[-2]['Open Time']        # Get the second to last timestamp (allow some overlap), if it exists.
         except:
+            utilities.throw_info('No Existing Data Found')
             startTime = datetime.datetime(2018, 1, 20).timestamp() * 1000        # If no existing data was found, use "Jan 20, 2018" in milliseconds as a default start datetime.
         endTime = datetime.datetime.now().timestamp() * 1000        # The end datetime will always be the current time in milliseconds.
 
         while startTime < endTime:
             curEnd = min(startTime + 259200000, endTime)        # Acquire data in 3 day intervals to not overload the API.
 
-            formatStart = str(datetime.datetime.fromtimestamp(startTime / 1e3))
-            formatEnd = str(datetime.datetime.fromtimestamp(curEnd / 1e3))
-            print('\tStart Time: ' + formatStart + '\tEnd Time: ' + formatEnd)
-
-            df = combine_frames(df, create_frame(client.get_historical_klines(coinPair, api, str(startTime), str(curEnd))))
+            df = combine_frames(df, create_frame(client.get_historical_klines(coinpair, api, str(startTime), str(curEnd))))
 
             startTime += 259200000        # Acquire data in 3 day intervals to not overload the API.
-            time.sleep(3)        # Wait between API calls to not overload the API.
+            time.sleep(2)        # Wait between API calls to not overload the API.
 
-        df.to_csv(dir + str(coinPair) + '.csv', index=False)
+        df.to_csv(dir + str(coinpair) + '.csv', index=False)
 
-    except:
-        print('ERROR: Unknown Error Getting Historical Data!')
-        sys.exit()
+    except Exception as e:
+        print(e)
+        utilities.throw_error('Unknown Error Getting Historical Data')
 
 
 if __name__ == "__main__":
 
-    print('INFO: Command Usage -> \'python3 get_history.py [CoinPair 1] [CoinPair 2] ... [CoinPair N]\'\n')
+    # Inform the start of this script.
+    utilities.throw_info('Get_History Script Starting')
 
-    locations = [{
-        'dir': 'data_15_min/',
-        'api': Client.KLINE_INTERVAL_15MINUTE
-    }, {
-        'dir': 'data_30_min/',
-        'api': Client.KLINE_INTERVAL_30MINUTE
-    }, {
-        'dir': 'data_1_hour/',
-        'api': Client.KLINE_INTERVAL_1HOUR
-    }, {
-        'dir': 'data_2_hour/',
-        'api': Client.KLINE_INTERVAL_2HOUR
-    }]
+    # Verify command usage.
+    if len(sys.argv) != 1: utilities.throw_error('Command Usage -> \'python3 get_history.py [coinpair 1] [coinpair 2] ... [coinpair N]\'')
 
-    coinPairs = sys.argv[1:]
-    for coinPair in ['ADAETH', 'BNBBTC', 'BNBETH', 'ETHBTC', 'XRPETH']:
-        if not coinPair in coinPairs: coinPairs.append(coinPair)
+    # Get the default info for time intervals and coinpairs to use.
+    locations = utilities.get_default_dirs_intervals()
+    coinpairs = utilities.get_default_coinpairs()
 
     for location in locations:
-        for coinPair in coinPairs:
-            print('INFO: Getting Data for ' + coinPair + ' CoinPair in ' + location['dir'])
-            get_data(location['dir'], location['api'], coinPair)
-            print('INFO: Done Getting Data for ' + coinPair + ' CoinPair in ' + location['dir'])
+        for coinpair in coinpairs:
+            utilities.throw_info('Getting Data for ' + coinpair + ' coinpair in ' + location['dir'])
+            get_data(location['dir'], location['api'], coinpair)
+            utilities.throw_info('Done Getting Data for ' + coinpair + ' coinpair in ' + location['dir'])
+
+    # Inform the end of this script
+    utilities.throw_info('Get_History Script Completed')
---
+++ utilities.py
@@ -0,0 +1,77 @@
+from binance.client import Client
+import sys, logging
+
+DIRS = ['data_15_min/', 'data_30_min/', 'data_1_hour/', 'data_2_hour/']
+INTERVALS = [Client.KLINE_INTERVAL_15MINUTE, Client.KLINE_INTERVAL_30MINUTE, Client.KLINE_INTERVAL_1HOUR, Client.KLINE_INTERVAL_2HOUR]
+DIRS_INTERVALS = [{
+    'dir': 'data_15_min/',
+    'api': Client.KLINE_INTERVAL_15MINUTE
+}, {
+    'dir': 'data_30_min/',
+    'api': Client.KLINE_INTERVAL_30MINUTE
+}, {
+    'dir': 'data_1_hour/',
+    'api': Client.KLINE_INTERVAL_1HOUR
+}, {
+    'dir': 'data_2_hour/',
+    'api': Client.KLINE_INTERVAL_2HOUR
+}]
+COINPAIRS = ['ADAETH', 'BNBBTC', 'BNBETH', 'ETHBTC', 'XRPETH']
+COLUMN_STRUCTURE = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number Trades', 'Taker Base Asset Volume', 'Take Quote Asset Volume', 'Ignore']
+
+
+def get_default_dirs():
+    return DIRS
+
+
+def get_default_intervals():
+    return INTERVALS
+
+
+def get_default_dirs_intervals():
+    return DIRS_INTERVALS
+
+
+def get_default_coinpairs():
+    return COINPAIRS
+
+
+# Setup the logging interface with the correct formatting and log file
+def set_logger():
+    logger = logging.getLogger('binance-bot')
+
+    # Output to the specific log file.
+    handler = logging.FileHandler('logs/bot.log')
+
+    # Create the default formatting for the logger and add the new handler.
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    # We will only use ERROR and INFO level logs.
+    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
+
+
+# Return the logging interface
+def get_logger():
+    # Be sure the logger is set before returning it.
+    logger = logging.getLogger('binance-bot')
+    if len(logger.handlers) == 0: set_logger()
+
+    return logging.getLogger('binance-bot')
+
+
+# Print an error message and exit the script
+# message - The unique error message
+def throw_error(message):
+    print('ERROR: ' + message)
+    get_logger().error(message)
+    sys.exit()
+
+
+# Print an info message
+# message - The unique info message
+def throw_info(message):
+    print('INFO: ' + message)
+    get_logger().info(message)
