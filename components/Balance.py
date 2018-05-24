4216f08ee0013493ac83330e52118ab6310b80c7
Alec Masterson <alecjmasterson@gmail.com>
2018-05-24 09:40:15 -0500

    Order Offline Mode - Balance Offline Fix

--- components/Balance.py
+++ components/Balance.py
@@ -1,9 +1,8 @@
 class Balance:
 
     # Initialize a new Balance with the required information
-    def __init__(self, client, online, asset, val):
+    def __init__(self, client, asset, val):
         self.client = client
-        self.online = True
         self.asset = asset
 
         # Update the amount available.
@@ -11,9 +10,10 @@ class Balance:
 
     # Update the free amount available for the asset
     # This chooses between online and offline mode
-    # val - The optional new amount of available asset
+    # Parameters are required online for offline mode
+    # val - The new amount of available asset
     def update(self, val):
-        if self.online: self.update_online()
+        if self.client != None: self.update_online()
         else self.update_offline(val)
 
     # The online updating function
--- components/Order.py
+++ components/Order.py
@@ -13,9 +13,19 @@ class Order:
         self.origQty = order['origQty']
         self.executedQty = order['executedQty']
 
-    # Update the order using the API to get the latest information
+    # Update the order using the latest information
+    # This chooses between online and offline mode
+    # Parameters are required online for offline mode
+    # executedQty - How much of the order has been filled
+    # status - The current status of the order
+    # time - The current time of the order
+    def update(self, executedQty, status, time):
+        if self.client != None: self.update_online()
+        else self.update_offline(executedQty, status, time)
+
+    # The online updating function
     # NOTE: This has an API call
-    def update(self):
+    def update_online(self):
 
         # Get the order information using the unique self.orderId value.
         updated = self.client.get_order(symbol=self.symbol, orderId=self.orderId)
@@ -24,3 +34,9 @@ class Order:
         self.executedQty = updated['executedQty']
         self.status = updated['status']
         self.time = updated['time']
+
+    # The offline updating function
+    def update_offline(self, executedQty, status, time):
+        self.executedQty = executedQty
+        self.status = status
+        self.time = time
