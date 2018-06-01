7043714480d626ddc5afa764fd371b2d26282e92
Trey Kind <trey.kind@returnpath.com>
2018-05-31 20:20:27 -0600

    Optimize working for 3 traits

--- optimize.py
+++ optimize.py
@@ -1,3 +1,4 @@
+import math
 import multiprocessing
 import random
 import time
@@ -11,23 +12,56 @@ from deap import algorithms, base, creator, tools
 import utilities
 from Bot import Bot
 
+eval_start_time = time.time()
 bot = Bot(False, True)
+print(time.time() - eval_start_time)
 
-# def normal_dist_list(seed):
-#     for i in range(100):
-#     yeild [np.random.normal(loc=x, scale=x**0.5) for x in seed]
+
+def ga_mutate(individual):
+
+    def rand_norm(num):
+        num = x
+        mag = 0
+        if x > 1:
+            while (num > 1):
+                mag += 1
+                num /= 10
+        elif x < 1:
+            while (num < 1):
+                mag += 1
+                num *= 10
+        return np.random.normal(loc=x, scale=mag)
+
+    while True:        # [1, 12] Integers
+        nr = int(rand_norm(individual[0]))
+        if 1 <= nr <= 12:
+            individual[0] = nr
+    while True:        # [1.0030, 1.0500] 4 Decimal places
+        nr = round(rand_norm(individual[1]), 4)
+        if 1.0030 <= nr <= 1.0500:
+            individual[1] = nr
+    while True:        # [0.000, 0.050] 3 Decimal places
+        nr = round(rand_norm(individual[2]), 3)
+        if 0.000 <= nr <= 0.050:
+            individual[2] = nr
+    while True:        # [0.9500, 0.9999] # 4 Decimal places
+        nr = round(rand_norm(individual[3]), 4)
+        if 0.9500 <= nr <= 0.9999:
+            individual[0] = nr
+
+    return individual,
 
 
 def ga_evaluate_genetics(individual):
-    eval_start_time = time.time()
+    # print('evaluate')
+    # eval_start_time = time.time()
     # print(individual)
     utilities.set_optimized(individual[0], individual[1], individual[2])
     # bot = Bot(False, True)
     # bot.print_optimize()
     fitness = bot.run_backtest(utilities.COINPAIRS[0])
     # print(fitness, time.time() - eval_start_time)
-    return (fitness,)
-    # bo = BayesianOptimization(lambda test: bot.run_backtest(utilities.COINPAIRS[0]), {'test': (3.0, 3.0)})
+    return fitness,
 
 
 def ga_optimize(seed=False, population_size=100, generations=100):
@@ -36,29 +70,30 @@ def ga_optimize(seed=False, population_size=100, generations=100):
 
     toolbox = base.Toolbox()
     if seed:
-        toolbox.register("individual", creator.Individual, [np.random.normal(loc=x, scale=x**0.5) for x in seed])
+        toolbox.register("individual", creator.Individual, ga_mutate(seed))
     else:
-        toolbox.register("attr_float", random.uniform, -5.0, 5.0)
-        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=3)
+        toolbox.register("attr_float", random.uniform, 0, 1.0)
+        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=4)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
     toolbox.register("evaluate", ga_evaluate_genetics)
     toolbox.register("mate", tools.cxTwoPoint)
-    toolbox.register("mutate", tools.mutGaussian, mu=0.0, sigma=0.5, indpb=0.5)
+    toolbox.register("mutate", ga_mutate)
     toolbox.register("select", tools.selSPEA2)
 
     # pool = multiprocessing.Pool()
     # toolbox.register("map", pool.map)
 
-    pop = toolbox.population(n=population_size * 5)
+    pop = toolbox.population(n=population_size * 2)
     hof = tools.HallOfFame(3)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", np.mean)
     stats.register("min", np.min)
     stats.register("max", np.max)
+    rt = tools.Statistics(lambda ind: ind.fitness.values)
 
     # pop, logbook = algorithms.eaSimple(pop, toolbox, cxpb=0.8, mutpb=0.4, ngen=1000, stats=stats, halloffame=hof, verbose=True)
-    pop, logbook = algorithms.eaMuPlusLambda(pop, toolbox, mu=int(population_size / 10), lambda_=population_size, cxpb=0.6, mutpb=0.25, ngen=generations, stats=stats, halloffame=hof, verbose=True)
+    pop, logbook = algorithms.eaMuPlusLambda(pop, toolbox, mu=int(population_size / 10), lambda_=population_size, cxpb=0.2, mutpb=0.75, ngen=generations, stats=stats, halloffame=hof, verbose=True)
 
     for b in hof:
         print("Top individual: %s\nwith fitness: %s" % (b, b.fitness))
@@ -70,11 +105,6 @@ def ga_optimize(seed=False, population_size=100, generations=100):
     plt.xlabel("Generation")
     plt.ylabel("Fitness")
     plt.legend(loc="lower right")
-
-    graph = networkx.DiGraph(history.genealogy_tree)
-    graph = graph.reverse()
-    colors = [toolbox.evaluate(history.genealogy_history[i])[0] for i in graph]
-    networkx.draw(graph, node_color=colors)
     plt.show()
 
 
@@ -86,4 +116,4 @@ if __name__ == "__main__":
     #
     # print(bo.res['max'])
     # ga_evaluate_genetics([1, 2, 3])
-    ga_optimize([9e5, 1.01, 0.003], 50, 50)
+    ga_optimize([3, 1.01, 0.003, 0.9975], 25, 5)
