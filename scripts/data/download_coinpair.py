142bca2aaa2ae8d042fb65f09ad0c14ae84a9d40
Alec Masterson <alecjmasterson@gmail.com>
2018-08-02 14:59:47 -0500

    Policies from DB

--- scripts/data/download_coinpair.py
+++ scripts/data/download_coinpair.py
@@ -18,17 +18,21 @@ if __name__ == '__main__':
     try:
         step = 0
 
-        logger.info('Downloading \'' + args.coinpair + '\' Historical Data from the DB...')
+        logger.info('Downloading \'' + args.coinpair + '\' Historical Data and Policies from the DB...')
         db_cursor.execute("SELECT * FROM " + args.coinpair)
         history = db_cursor.fetchall()
+        db_cursor.execute("SELECT * FROM POLICIES")
+        policies = db_cursor.fetchall()
         step += 1
 
-        logger.info('Writing \'' + args.coinpair + '\' Historical Data to File...')
+        logger.info('Writing \'' + args.coinpair + '\' Historical Data and Policies to File...')
         data = pandas.DataFrame(history, columns=utilities.HISTORY_STRUCTURE)
         data.to_csv('data/history/' + args.coinpair + '.csv', index=False)
+        policies = pandas.DataFrame(policies, columns=utilities.POLICY_STRUCTURE)
+        policies.to_csv('data/policies.csv', index=False)
     except:
-        if step == 0: logger.error('Failed to Download \'' + args.coinpair + '\' Historical Data from the DB')
-        if step == 1: logger.error('Failed to Write \'' + args.coinpair + '\' Historical Data to File')
+        if step == 0: logger.error('Failed to Download \'' + args.coinpair + '\' Historical Data and Policies from the DB')
+        if step == 1: logger.error('Failed to Write \'' + args.coinpair + '\' Historical Data and Policies to File')
         error = True
 
     helpers.db_disconnect(db, logger)
