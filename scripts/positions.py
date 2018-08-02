19125268dabc6b4fc67114cdd8f034603af9f03a
Alec Masterson <alecjmasterson@gmail.com>
2018-08-01 20:32:01 -0500

    File Cleanup

--- scripts/positions.py
+++
@@ -1,7 +0,0 @@
-'''
-self.age = time - self.time
-self.current = price
-self.result = self.current / self.price
-if self.result > self.peak: self.peak = self.result
-if self.peak > utilities.STOP_LOSS_ARM and self.peak - self.result > utilities.STOP_LOSS: self.stopLoss = True
-'''
