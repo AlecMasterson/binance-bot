79ac77cdbef8818168151497173522bc17d8c118
Alec Masterson <alecjmasterson@gmail.com>
2018-08-06 11:46:25 -0500

    Hard Stop on Error

--- scripts/data/update_balances.py
+++ scripts/data/update_balances.py
@@ -9,20 +9,20 @@ logger = helpers.create_logger('update_balances')
 
 if __name__ == '__main__':
     argparse.ArgumentParser(description='Used for Updating the DB with Current Balances').parse_args()
-    errors = 0
+    error = True
 
     client = helpers.binance_connect(logger)
     db, db_cursor = helpers.db_connect(logger)
 
     logger.info('Starting Infinite Loop... Hold On Tight!')
-    while errors < 10:
+    while not error:
         balances = {}
         for coinpair in utilities.COINPAIRS:
             try:
                 logger.info('Getting \'' + coinpair[:-3] + '\' Balance from the Binance API...')
                 balances[coinpair[:-3]] = client.get_asset_balance(asset=coinpair[:-3])['free']
             except:
-                errors += 1
+                error = True
                 logger.error('Failed to Get \'' + coinpair[:-3] + '\' Balance from the Binance API')
 
         for asset in balances:
@@ -31,10 +31,10 @@ if __name__ == '__main__':
                 db_cursor.execute("INSERT INTO BALANCES VALUES ('" + asset + "', " + str(balances[asset]) + ") ON CONFLICT (ASSET) DO UPDATE SET FREE = Excluded.FREE;")
                 db.commit()
             except:
-                errors += 1
+                error = True
                 logger.error('Failed to Update \'' + asset + '\' Balance into the DB')
 
         time.sleep(15)
 
     helpers.db_disconnect(db, logger)
-    if errors != 0: sys.exit(1)
+    if error: sys.exit(1)
