16d7813133d958245c50591e128643bc5fd67b4f
Alec Masterson <alecjmasterson@gmail.com>
2018-08-09 14:45:33 -0500

    Getting Balances in Main

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ def get_data(db_cursor):
     try:
         logger.info('Downloading \'BNBBTC\' Historical Data from the DB...')
         db_cursor.execute("SELECT * FROM BNBBTC ORDER BY OPEN_TIME DESC LIMIT " + str(utilities.WINDOW * 2) + ";")
-        data = pandas.DataFrame(db_cursor.fetchall()[::-1], columns=utilities.HISTORY_STRUCTURE)
-        return data
+        return pandas.DataFrame(db_cursor.fetchall()[::-1], columns=utilities.HISTORY_STRUCTURE)
     except:
         logger.error('Failed to Download \'BNBBTC\' Historical Data from the DB')
+        return None
+
+
+def get_balances(db_cursor):
+    try:
+        logger.info('Downloading Balances from the DB...')
+        db_cursor.execute("SELECT * FROM BALANCES;")
+        return pandas.DataFrame(db_cursor.fetchall(), columns=utilities.BALANCES_STRUCTURE)
+    except:
+        logger.error('Failed to Download Balances from the DB')
         return None
 
 
@@ -26,8 +35,9 @@ if __name__ == '__main__':
     logger.info('Starting Infinite Loop... Hold On Tight!')
     while not error:
         data = get_data(db_cursor)
+        if data is not None: balances = get_balances(db_cursor)
 
-        if data is None: error = True
+        if data is None or balances is None: error = True
         else:
             action = 'HOLD'        #strategy.action(data)
             logger.info('Bot Chooses Action: ' + action)
