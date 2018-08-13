d2b87e40ee27435ea1e1165227d7b49bbb00ed05
Alec Masterson <alecjmasterson@gmail.com>
2018-08-13 10:35:10 -0500

    Account Balances Cron Job

--- scripts/data/update_balances.py
+++ scripts/data/update_balances.py
@@ -1,42 +1,36 @@
-import argparse, time, sys, os, traceback
+import argparse, sys, os
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/..'))
-import helpers
+import helpers, helpers_binance, helpers_db
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + './..'))
 import utilities
 
 logger = helpers.create_logger('update_balances')
 
 if __name__ == '__main__':
-    argparse.ArgumentParser(description='Used for Updating the DB with Current Balances').parse_args()
+    argparse.ArgumentParser(description='Used for Updating the DB with Current Asset Balances').parse_args()
     error = False
 
-    client = helpers.binance_connect(logger)
-    db, db_cursor = helpers.db_connect(logger)
+    logger.info('Updating All Asset Balances in the DB...')
+    try:
+        client = helpers_binance.binance_connect(logger)
+        if client is None: raise Exception
+        db, db_cursor = helpers_db.db_connect(logger)
+        if db is None or db_cursor is None: raise Exception
 
-    logger.info('Starting Infinite Loop... Hold On Tight!')
-    while not error:
         balances = {}
         for coinpair in utilities.COINPAIRS:
-            try:
-                logger.info('Getting \'' + coinpair[:-3] + '\' Balance from the Binance API...')
-                balances[coinpair[:-3]] = client.get_asset_balance(asset=coinpair[:-3])['free']
-            except:
-                error = True
-                logger.error('\n' + traceback.print_exc())
-                logger.error('Failed to Get \'' + coinpair[:-3] + '\' Balance from the Binance API')
+            balances[coinpair[:-3]] = helpers_binance.binance_get_asset_balance(client, coinpair[:-3], logger)
+            if balances[coinpair[:-3]] is None: raise Exception
 
         for asset in balances:
-            try:
-                logger.info('Updating \'' + asset + '\' Balance into the DB...')
-                db_cursor.execute("INSERT INTO BALANCES VALUES ('" + asset + "', " + str(balances[asset]) + ") ON CONFLICT (ASSET) DO UPDATE SET FREE = Excluded.FREE;")
-                db.commit()
-            except:
-                error = True
-                logger.error('\n' + traceback.print_exc())
-                logger.error('Failed to Update \'' + asset + '\' Balance into the DB')
+            result = helpers_db.db_insert_asset_balance(db_cursor, asset, balances[asset], logger)
+            if result is None: raise Exception
+    except:
+        logger.error('Failed to Update All Balances in the DB')
+        error = True
 
-        time.sleep(15)
+    helpers_db.db_disconnect(db, logger)
 
-    helpers.db_disconnect(db, logger)
     if error: sys.exit(1)
+    sys.exit(0)
