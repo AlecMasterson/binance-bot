725d1b92be9ba46e480f48250aa99e4b44ba8688
Alec Masterson <alecjmasterson@gmail.com>
2018-08-13 10:34:39 -0500

    Binance Helpers Print the Traceback
    all return None on error
    downloading asset balance added

--- scripts/helpers_binance.py
+++ scripts/helpers_binance.py
@@ -1,4 +1,4 @@
-import pandas, sys, os
+import pandas, sys, os, traceback
 from binance.client import Client
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + './..'))
@@ -11,7 +11,8 @@ def binance_connect(logger):
         return Client(utilities.PUBLIC_KEY, utilities.SECRET_KEY)
     except:
         logger.error('Failed to Connect to the Binance API. Ending Script...')
-        sys.exit(1)
+        logger.error('\n' + traceback.print_exc())
+        return None
 
 
 def binance_get_coinpair(client, coinpair, logger):
@@ -23,6 +24,7 @@ def binance_get_coinpair(client, coinpair, logger):
         return pandas.DataFrame(data, columns=utilities.HISTORY_STRUCTURE)
     except:
         logger.error('Failed to Download \'' + coinpair + '\' Historical Data from the Binance API')
+        logger.error('\n' + traceback.print_exc())
         return None
 
 
@@ -32,4 +34,15 @@ def binance_get_coinpair_policy(client, coinpair, logger):
         return client.get_symbol_info(coinpair)
     except:
         logger.error('Failed to Download \'' + coinpair + '\' Trading Policy from the Binance API')
+        logger.error('\n' + traceback.print_exc())
+        return None
+
+
+def binance_get_asset_balance(client, asset, logger):
+    try:
+        logger.info('Downloading \'' + coinpair[:-3] + '\' Balance from the Binance API...')
+        return client.get_asset_balance(asset=coinpair[:-3])['free']
+    except:
+        logger.error('Failed to Download \'' + coinpair[:-3] + '\' Balance from the Binance API')
+        logger.error('\n' + traceback.print_exc())
         return None
