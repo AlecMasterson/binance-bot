bce6c6e05e6eed4d63cc823fe830e1013b8d4f68
Alec Masterson <alecjmasterson@gmail.com>
2018-08-14 14:59:24 -0500

    woops

--- scripts/data/download_coinpair.py
+++ scripts/data/download_coinpair.py
@@ -7,20 +7,20 @@ logger = helpers.create_logger('download_coinpair')
 
 
 def fun(coinpair):
-    db_info = helpers_db.db_connect(logger)
+    db_info = helpers_db.safe_connect(logger)
     if db_info is None: raise Exception
 
-    data = helpers_db.safe_get_historical_data(logger, db_cursor, coinpair)
+    data = helpers_db.safe_get_historical_data(logger, db_info[1], coinpair)
     if data is None: raise Exception
 
-    if helpers.to_file(data, 'data/history/' + coinpair + '.csv', logger) is None: raise Exception
+    if helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data) is None: raise Exception
 
-    policies = helpers_db.safe_get_trading_policies(logger, db_cursor)
+    policies = helpers_db.safe_get_trading_policies(logger, db_info[1])
     if policies is None: raise Exception
 
-    if helpers.to_file(policies, 'data/policies.csv', logger) is None: raise Exception
+    if helpers.safe_to_file(logger, 'data/policies.csv', policies) is None: raise Exception
 
-    return True
+    return helpers_db.safe_disconnect(logger, db_info[0])
 
 
 if __name__ == '__main__':
@@ -30,6 +30,4 @@ if __name__ == '__main__':
 
     result = helpers.bullet_proof(logger, 'Downloading Coinpair \'' + args.coinpair + '\' from the DB', lambda: fun(args.coinpair))
     if result is None: sys.exit(1)
-    else:
-        if helpers_db.safe_disconnect(logger, db) is None: sys.exit(1)
-        sys.exit(0)
+    else: sys.exit(0)
