ce5dee070d38da7eff53e5c87ff287e62f810d8c
Alec Masterson <alecjmasterson@gmail.com>
2018-08-24 09:15:50 -0500

    One Download Script

--- scripts/data/download_data.py
+++ scripts/data/download_data.py
@@ -3,12 +3,17 @@ sys.path.append(os.path.join(os.getcwd(), 'binance-bot'))
 sys.path.append(os.path.join(os.path.join(os.getcwd(), 'binance-bot'), 'scripts'))
 import helpers, helpers_db
 
-logger = helpers.create_logger('download_policies')
+logger = helpers.create_logger('download_history')
 
 
 def fun():
     db = helpers_db.safe_connect(logger)
     if db is None: return None
+
+    for coinpair in utilities.COINPAIRS:
+        data = helpers_db.safe_get_historical_data(logger, db, coinpair)
+        if data is None: return None
+        if helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data) is None: return None
 
     policies = helpers_db.safe_get_trading_policies(logger, db)
     if policies is None: return None
@@ -17,9 +22,9 @@ def fun():
 
 
 if __name__ == '__main__':
-    argparse.ArgumentParser(description='Used for Downloading Trading Policies from the DB').parse_args()
+    argparse.ArgumentParser(description='Used for Downloading All Data from the DB').parser.parse_args()
 
-    message = 'Downloading Trading Policies from the DB'
+    message = 'Downloading All Data from the DB'
     result = helpers.bullet_proof(logger, message, lambda: fun())
     if result is None: sys.exit(1)
     else:
