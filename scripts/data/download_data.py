0ceeee94c3faf2ff3a13698abe6f6f06945d02ee
Alec Masterson <alecjmasterson@gmail.com>
2018-09-11 11:32:31 -0500

    Updated Downloading Script

--- scripts/data/download_data.py
+++ scripts/data/download_data.py
@@ -6,27 +6,23 @@ import helpers, helpers_db
 logger = helpers.create_logger('download_data')
 
 
-def fun():
-    db = helpers_db.safe_connect(logger)
-    if db is None: return None
-
+def fun(db):
     for coinpair in utilities.COINPAIRS:
         data = helpers_db.safe_get_historical_data(logger, db, coinpair)
-        if data is None: return None
-        if helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data) is None: return None
+        if data is None: return 1
 
-    policies = helpers_db.safe_get_trading_policies(logger, db)
-    if policies is None: return None
+        if helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data) is None: return 1
 
-    return helpers.safe_to_file(logger, 'data/policies.csv', policies)
+    return 0
 
 
 if __name__ == '__main__':
     argparse.ArgumentParser(description='Used for Downloading All Data from the DB').parser.parse_args()
 
-    message = 'Downloading All Data from the DB'
-    result = helpers.bullet_proof(logger, message, lambda: fun())
-    if result is None: sys.exit(1)
-    else:
-        logger.info('SUCCESS - ' + message)
-        sys.exit(0)
+	db = helpers_db.safe_connect(logger)
+    if db is None: sys.exit(1)
+
+    exit_status = helpers.bullet_proof(logger, 'Downloading All Data from the DB', lambda: fun(db))
+
+	logger.error('Closing Script with Exit Status ' + str(exit_status))
+    sys.exit(exit_status)
