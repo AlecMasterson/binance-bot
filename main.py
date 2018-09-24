d655fdfbbfb428aa1895a8c24981eb45e0cfeacb
Alec Masterson <alecjmasterson@gmail.com>
2018-09-24 11:44:36 -0500

    Updating ORDERS Table

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ logger = helpers.create_logger('main')
 
 
 def fun(client, db):
-    actions = helpers_db.safe_get_bot_actions(logger, db)
+    actions = helpers_db.safe_get_table(logger, db, 'ACTIONS')
     if actions is None: return 1
 
     for coinpair in utilities.COINPAIRS:
@@ -15,7 +15,9 @@ def fun(client, db):
 
         coinpair_actions = actions[actions['COINPAIR'] == coinpair]
         if len(coinpair_actions) > 0: action = coinpair_actions.iloc[len(coinpair_actions) - 1]
-        else: continue
+        else:
+            logger.warn('No \'' + coinpair + '\' Actions')
+            continue
 
         if int(round(time.time() * 1000)) - action['TIME'] > utilities.ACTION_RECENT:
             logger.warn('Most Recent \'' + coinpair + '\' Action Not Recent Enough')
@@ -32,8 +34,17 @@ def fun(client, db):
             if balance is None: continue
 
             quantity, price = helpers.validate_order(trading_policy, action['ACTION'], float(balance), float(action['PRICE']))
-            if quantity != -1 and price != -1: helpers_binance.safe_create_order(logger, client, coinpair, action['ACTION'], quantity, price)
-            else: logger.warn('Failed to Create Valid Order with Current Balance')
+            if quantity != -1 and price != -1:
+                orders = helpers_db.safe_get_table(logger, db, 'ORDERS', utilities.ORDERS_STRUCTURE)
+                if orders is None: return 1
+
+                result = helpers_binance.safe_create_order(logger, client, coinpair, action['ACTION'], quantity, price)
+                if result is None: return 1
+
+                orders.append({'ID': result['orderId'], 'TIME': result['transactTime'], 'STATUS': 'NEW'}, ignore_index=True)
+                if helpers_db.safe_create_table(logger, db, 'ORDERS', orders) is None: return 1
+            else:
+                logger.warn('Failed to Validate Order with Current Balance')
 
     return 0
 
