3fdd3133d7d06306e0524c3d989fb483c47936a9
Alec Masterson <alecjmasterson@gmail.com>
2018-10-03 12:36:44 -0500

    Extras Update

--- scripts/data/erase_coinpair_history.py
+++ scripts/data/erase_coinpair_history.py
@@ -23,7 +23,7 @@ def fun(**args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Used for Erasing a Coinpair\'s History in the DB')
     parser.add_argument('-c', '--coinpair', help='the Coinpair to Erase', type=str, dest='coinpair', required=True)
-    parser.add_argument('-t', help='the time interval', type=str, dest='time_interval', required=True, choices=['5m', '30m', '1h', '2h', '4h'])
+    parser.add_argument('-t', help='the time interval', type=str, dest='time_interval', required=True, choices=utilities.TIME_INTERVALS)
     args = parser.parse_args()
 
     if input('Are you sure you want to erase all \'' + args.coinpair + '\' history from the DB? (y) ') != 'y': sys.exit(1)
