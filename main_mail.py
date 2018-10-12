cd793203701adfcf593be558fd55a3363c17d859
Alec Masterson <alecjmasterson@gmail.com>
2018-10-12 14:19:06 -0500

    Untested Email Service

--- main_mail.py
+++ main_mail.py
@@ -1,4 +1,5 @@
 import sys, os, argparse, smtplib
+from email.mime.text import MIMEText
 sys.path.append(os.path.join(os.getcwd(), 'binance-bot'))
 sys.path.append(os.path.join(os.path.join(os.getcwd(), 'binance-bot'), 'scripts'))
 import utilities, helpers, helpers_db, signals
@@ -7,14 +8,31 @@ logger = helpers.create_logger('main_email')
 
 
 def fun(**args):
+
+    potential = []
     for coinpair in utilities.COINPAIRS:
         data = helpers_db.safe_get_table(logger, args['db'], coinpair, utilities.HISTORY_STRUCTURE)
         if data is None: return 1
 
         data = data[data['INTERVAL'] == '15m']
 
-        if signals.rsi(data) and signals.lowerband(data):
-            logger.info('Sending Email Notification for \'' + coinpair + '\'')
+        if signals.rsi(data) and signals.lowerband(data): potential.append(coinpair)
+
+    if len(potential) > 0:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(utilities.EMAIL_USERNAME, utilities.EMAIL_PASS)
+
+        logger.info('Sending Email Notification for \'' + str(potential) + '\'')
+        msg = MIMEText('Potential Buy Orders on: ' + str(potential))
+        msg['Subject'] = 'Binance-Bot'
+        msg['From'] = utilities.EMAIL_USERNAME
+        msg['To'] = utilities.EMAIL_USERNAME
+
+        server.send_message(msg)
+
+        server.quit()
 
     return 0
 
--- utilities.py
+++ utilities.py
@@ -32,6 +32,9 @@ DB_PORT = '3306'
 DB_USER = 'epsenex'
 DB_PASS = 'Minecraft#1PUBG#2'
 
+EMAIL_USERNAME = 'alecjmasterson@gmail.com'
+EMAIL_PASS = 'tmnvzdlgdnntaavh'
+
 # 240000ms = 4min
 ACTION_RECENT = 240000
 ORDER_TIME_LIMIT = 240000
