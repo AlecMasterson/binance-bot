4d9b8806b6bec5da4db4cb742f45d298f0ca5891
Alec Masterson <alecjmasterson@gmail.com>
2018-10-13 18:59:42 -0500

    More control in Downloading

--- scripts/data/download_history.py
+++ scripts/data/download_history.py
@@ -6,17 +6,28 @@ import utilities, helpers, helpers_db
 logger = helpers.create_logger('download_history')
 
 
-def fun(**args):
-    for coinpair in utilities.COINPAIRS:
-        data = helpers_db.safe_get_table(logger, args['db'], coinpair, utilities.HISTORY_STRUCTURE)
-        if data is None: continue
+def download(db, coinpair):
+    data = helpers_db.safe_get_table(logger, db, coinpair, utilities.HISTORY_STRUCTURE)
+    if data is None: return False
 
-        helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data)
+    if helpers.safe_to_file(logger, 'data/history/' + coinpair + '.csv', data) is None: return False
+
+    return True
+
+
+def fun(**args):
+    if not args['extra']['coinpair'] is None:
+        if not download(args['db'], args['extra']['coinpair']): return 1
+    else:
+        for coinpair in utilities.COINPAIRS:
+            if not download(args['db'], coinpair): return 1
 
     return 0
 
 
 if __name__ == '__main__':
-    argparse.ArgumentParser(description='Used for Downloading All History from the DB').parse_args()
+    parser = argparse.ArgumentParser(description='Used for Downloading All History from the DB')
+    parser.add_argument('-c', '--coinpair', help='a specific coinpair to download', type=str, dest='coinpair', required=False)
+    args = parser.parse_args()
 
-    helpers.main_function(logger, 'Downloading All History from the DB', fun, db=True)
+    helpers.main_function(logger, 'Downloading {} History from the DB'.format('All' if args.coinpair is None else '\'' + args.coinpair + '\''), fun, db=True, extra={'coinpair': args.coinpair})
