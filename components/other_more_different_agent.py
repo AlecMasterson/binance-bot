afc1b4e7db8d4ad714793d087ee697c021328115
Trey Kind <trey.kind@returnpath.com>
2018-11-10 18:06:16 -0700

    From_spec sucks

--- components/other_more_different_agent.py
+++ components/other_more_different_agent.py
@@ -1,13 +1,15 @@
 from collections import deque
-import sys, os
+import sys, os, json
 from copy import deepcopy
 sys.path.append(os.path.join(os.getcwd(), 'binance-bot'))
 sys.path.append(os.path.join(os.getcwd(), 'binance-bot', 'scripts'))
 sys.path.append(os.path.join(os.getcwd(), 'binance-bot', 'components'))
+sys.path.append(os.path.join(os.getcwd(), 'binance-bot', 'agents'))
 import utilities, helpers, signals
 from Backtest import Backtest
 
 import numpy as np
+from tensorforce.agents import Agent as tf_agent
 from tensorforce.agents import DQNAgent
 
 
@@ -18,6 +20,7 @@ class Agent:
     windows = {}
     i = 0
     coinpairs = []
+    balance = 0
 
     def __init__(self, coinpairs):
         self.coinpairs = coinpairs
@@ -25,36 +28,66 @@ class Agent:
             temp_data = helpers.read_file('data/history/' + coinpair + '.csv')
             if temp_data is None: return 1
 
-            self.saved_data[coinpair] = temp_data[temp_data['INTERVAL'] == utilities.BACKTEST_CANDLE_INTERVAL_STRING].sort_values(by=['OPEN_TIME']).reset_index(drop=True)
+            self.data[coinpair] = temp_data[temp_data['INTERVAL'] == utilities.BACKTEST_CANDLE_INTERVAL_STRING].sort_values(by=['OPEN_TIME']).reset_index(drop=True)
             self.windows[coinpair] = deque(maxlen=1)
 
-        self.data = deepcopy(self.saved_data)
-        self.backtest = Backtest(self.step)
-        # Create a Proximal Policy Optimization agent
-        self.brain = DQNAgent(states=dict(type='float', shape=(len(self.coinpairs)*18,)), actions=dict(type='int', num_actions=3), network=[dict(type='dense', size=64), dict(type='dense', size=64)])
+        self.backtest = Backtest(self.step, self.data)
+        self.i = 0
+
+        with open('binance-bot/agents/tf_configs/dqn_relu_network.json', 'r') as fp:
+            agent_config = json.load(fp=fp)
+        with open('binance-bot/agents/tf_configs/dqn_relu_network.json', 'r') as fp:
+            network_spec = json.load(fp=fp)
+        # self.brain = tf_agent.from_spec(
+        #     spec=agent_config, kwargs={
+        #         'states': {
+        #             'type': 'float',
+        #             'shape': (len(self.coinpairs) * 18,)
+        #         },
+        #         'actions': {
+        #             'type': 'int',
+        #             'num_actions': len(self.coinpairs) + 1
+        #         },
+        #         'network': network_spec
+        #     }
+        # )
+        # self.brain = tf_agent.from_spec(spec=agent_config, kwargs=dict(states=np.array(len(self.coinpairs) * 18,), actions=np.array(len(self.coinpairs) + 1,), network=network_spec))
+        self.brain = DQNAgent(
+            states=dict(type='float', shape=(len(self.coinpairs) * 18,)),
+            actions=dict(type='int', num_actions=len(self.coinpairs) + 1),
+            network=[dict(type='dense', size=64), dict(type='dense', size=64)]
+        )
 
     def reset(self):
+        self.i = 0
         for coinpair in self.coinpairs:
             self.windows[coinpair] = deque(maxlen=1)
-        self.data = deepcopy(self.saved_data)
         self.backtest.reset()
 
     def run(self):
-        return self.backtest.backtest(self.data)
+        return self.backtest.backtest()
 
     def step(self, data):
+        self.i += 1
         self.remember(data)
-        # print(self.consider().shape)
         action = self.brain.act(self.consider())
-        actions = [False]*len(self.coinpairs)
-        actions[action] = True
+        actions = [False] * len(self.coinpairs)
+        try:
+            actions[action] = True
+        except:
+            pass
         actions = self.map_actions(actions)
-        self.brain.observe(reward=0, terminal=False)
+        # reward = (data['BALANCE'] - self.balance)/10.0
+        # self.balance = data['BALANCE']
+        # print(self.balance, reward)
+        if self.i <= 1440:
+            self.brain.observe(reward=0, terminal=False)
         return actions
 
     def remember(self, data):
         for k in data:
-            self.windows[k].append(data[k])
+            if k != 'BALANCE':
+                self.windows[k].append(data[k])
 
     def consider(self):
         return [d[n] for k in self.windows for d in self.windows[k] for n in d if n != 'INTERVAL']
@@ -67,10 +100,9 @@ class Agent:
 
 
 if __name__ == '__main__':
-    agent = Agent(['ADABTC', 'XMRBTC'])
+    agent = Agent(['ADABTC'])
     for x in range(10):
         result = agent.run()
-        print(result, len(agent.consider()))
+        print(x, result)
         agent.brain.observe(reward=(result - utilities.STARTING_BALANCE), terminal=True)
         agent.reset()
-
