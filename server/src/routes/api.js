f4ac92db67804aff6b82e4232c33b78c734542ef
Alec Masterson <alecjmasterson@gmail.com>
2020-02-17 12:31:08 -0600

    [server] - Additional API for counts.
    m

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const History = require('../services/History');
 
 router.get('/test', function (req, res) {
     res.send('Hello from the Binance Bot!');
+});
+
+router.post('/history/count', async function (req, res) {
+    const count = await History.getCount(req.query.symbol);
+    res.send(`Count: ${count}`);
 });
 
 module.exports = router;
